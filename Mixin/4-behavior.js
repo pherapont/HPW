2313a5033c805a48ebd4fad5d3aeb8008673eb04
pherapont <pherapont@yandex.ru>
2020-10-28 10:51:18 +0700

    Mixin for changing behavior of object, not his structure.

---
+++ Mixin/4-behavior.js
@@ -0,0 +1,27 @@
+'use strict';
+
+//  Миксины могут изменять поведение объекта не изменяя его структуры
+//  Это по сути wrappers методов
+
+const scalable = image => {
+	image.scale = () => console.log('Image scaled');
+};
+
+const lazy = image => {
+	const scale = image.scale;
+	image.scale = () => setImmediate(() => scale());
+};
+
+const image = {};
+
+console.log('Mixin scalable() adds method: scale');
+scalable(image);
+console.log('Before scale', image);
+image.scale();
+console.log('After scale\n');
+
+console.log('Mixin lazy() adds lazy behavior');
+lazy(image);
+console.log('Before scale', image);
+image.scale();
+console.log('After scale');
